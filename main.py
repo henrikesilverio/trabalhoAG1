71d9f6e8251d51e76bac10aec7b8d2667809696d
henrikesilverio <henrikesilverio@users.noreply.github.com>
2018-04-21 14:29:09 -0300

    Busca em largura e impressão do caminho/distancia entre dois vertice

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 arquivo = open("palavras.txt")
 
 palavras = arquivo.read().split("\n")
-grafo = []
+grafo = {}
 tempo = 0
 
 # Construindo o grafo
@@ -18,19 +18,60 @@ def diferemEmUmaPosicao(primeiro, segundo):
 
 
 for palavra in palavras:
-    grafo.append({
+    grafo[palavra] = {
         "nome": palavra,
         "cor": "branco",
-        "pred": {},
-        "inicio": 0,
-        "termino": 0,
+        "distancia": 999999999,
+        "predecessor": {},
+        "tempo_inicio": 0,
+        "tempo_termino": 0,
         "adjacentes": []
-    })
+    }
 
-for i in range(0, len(grafo)):
-    for j in range(0, len(grafo)):
-        if grafo[i]["nome"] != grafo[j]["nome"] and diferemEmUmaPosicao(grafo[i]["nome"], grafo[j]["nome"]):
-            grafo[i]["adjacentes"].append(grafo[j])
+for vertice_i in grafo.values():
+    for vertice_j in grafo.values():
+        if vertice_i["nome"] != vertice_j["nome"] and diferemEmUmaPosicao(vertice_i["nome"], vertice_j["nome"]):
+            vertice_i["adjacentes"].append(vertice_j)
+
+# Busca em largura
+# Calcular a distancia entre duas palavras quaisquer e
+# Imprimir o caminho entre elas
+
+
+def WFS(grafo, verticeInicial):
+    verticeInicial["cor"] = "cinza"
+    verticeInicial["distancia"] = 0
+    fila = []
+    fila.append(verticeInicial)
+    while(len(fila) != 0):
+        vertice = fila.pop(0)
+        for adjacente in vertice["adjacentes"]:
+            if adjacente["cor"] == "branco":
+                adjacente["cor"] = "cinza"
+                adjacente["distancia"] = vertice["distancia"] + 1
+                adjacente["predecessor"] = vertice
+                fila.append(adjacente)
+        vertice["cor"] = "preto"
+
+# Imprimir o caminho do vertice inical até o vertice final
+
+
+def ImprimirCaminho(verticeInicial, verticeFinal):
+    if verticeFinal["nome"] == verticeInicial["nome"]:
+        print(verticeInicial["nome"])
+    elif not verticeFinal["predecessor"]:
+        print("não existe caminho de {0} a {1}".format(
+            verticeInicial["nome"], verticeFinal["nome"]))
+    else:
+        ImprimirCaminho(verticeInicial, verticeFinal["predecessor"])
+        print(verticeFinal["nome"])
+
+
+WFS(grafo, grafo[palavras[0]])
+ImprimirCaminho(grafo[palavras[0]], grafo[palavras[1]])
+print("a distância entre {0} a {1} e de {2}".format(grafo[palavras[0]]["nome"],
+                                                    grafo[palavras[1]]["nome"],
+                                                    grafo[palavras[1]]["distancia"]))
 
 # Busca em profundidade
 
@@ -39,19 +80,20 @@ def DFSVisit(vertice):
     global tempo
     tempo += + 1
     vertice["cor"] = "cinza"
-    vertice["inicio"] = tempo
+    vertice["tempo_inicio"] = tempo
     for adjacente in vertice["adjacentes"]:
         if adjacente["cor"] == "branco":
-            adjacente["pred"] = vertice
+            adjacente["predecessor"] = vertice
             DFSVisit(adjacente)
     vertice["cor"] = "Preto"
     tempo += 1
-    vertice["termino"] = tempo
+    vertice["tempo_termino"] = tempo
 
 
 def DFS(grafo, verticeInicial):
-    for vertice in grafo:
+    for vertice in grafo.values():
         if vertice["cor"] == "branco":
             DFSVisit(vertice)
 
-DFS(grafo, grafo[0])
+
+# DFS(grafo, grafo[palavras[0]])
